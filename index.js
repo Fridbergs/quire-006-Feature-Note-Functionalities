3bb35c9020a8d73bfbd3c8dcc84892623961b58a
Fridbergs <144354526+Fridbergs@users.noreply.github.com>
2024-01-31 13:25:55 +0100

    second commit

--- index.js
+++ index.js
@@ -1,40 +1,25 @@
-document.getElementById("fileInput").addEventListener("change", function (e) {
-  let file = e.target.files[0]; // Hämta den valda filen
+document.getElementById("uploadButton").addEventListener("click", function () {
+  document.getElementById("fileInput").click(); // Triggar klick på den dolda filväljaren
+});
 
+document.getElementById("fileInput").addEventListener("change", function (e) {
+  let file = e.target.files[0];
   if (!file.type.startsWith("image/")) {
     return;
   } // Se till att det är en bildfil
 
-  let img = document.createElement("img");
-  img.classList.add("image");
-  img.file = file;
-
-  // Läs in och visa bilden
   let reader = new FileReader();
-  reader.onload = (function (aImg) {
-    return function (e) {
-      aImg.src = e.target.result;
-    };
-  })(img);
+  reader.onload = function (e) {
+    let img = document.createElement("img");
+    img.src = e.target.result;
 
+    img.style.maxwidth = "200px"; // Exempelstorlek, kan ändras
+    img.style.height = "auto";
+
+    img.style.resize = "both";
+    img.style.overflow = "auto";
+
+    document.getElementById("editableDiv").appendChild(img);
+  };
   reader.readAsDataURL(file);
-
-  // Lägg till bilden i container
-  let container = document.getElementById("imageContainer");
-
-  container.appendChild(img);
-});
-
-document.getElementById("customButton").addEventListener("click", function () {
-  document.getElementById("fileInput").click();
-});
-
-document.getElementById("fileInput").addEventListener("change", function () {
-  let fileInput = document.getElementById("fileInput");
-  let chosenFile = fileInput.files[0];
-  if (chosenFile) {
-    document.getElementById("fileChosen").textContent = chosenFile.name;
-  } else {
-    document.getElementById("fileChosen").textContent = "Ingen fil vald";
-  }
 });
