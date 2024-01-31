15db0177ee58d24309f7756a71fde4fe977d5b00
Fridbergs <144354526+Fridbergs@users.noreply.github.com>
2024-01-31 14:40:12 +0100

    movable image

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
-document.getElementById("uploadButton").addEventListener("click", function () {
-  document.getElementById("fileInput").click(); // Triggar klick på den dolda filväljaren
-});
+//Kod för att kunna flytta på bilden.
+const resizableImage = document.getElementById("resizableImage");
 
-document.getElementById("fileInput").addEventListener("change", function (e) {
-  let file = e.target.files[0];
-  if (!file.type.startsWith("image/")) {
-    return;
-  } // Se till att det är en bildfil
-
-  let reader = new FileReader();
-  reader.onload = function (e) {
-    let img = document.createElement("img");
-    img.src = e.target.result;
-
-    img.style.maxWidth = "100%"; // Exempelstorlek, kan ändras
-    img.style.MaxHeight = "100%";
-    img.style.height = "auto";
-
-    img.style.resize = "both";
-    img.style.overflow = "auto";
-
-    document.getElementById("editableDiv").appendChild(img);
-  };
-  reader.readAsDataURL(file);
+resizableImage.addEventListener("input", function () {
+  // Uppdatera bildens storlek baserat på användarens ändringar
+  const newWidth = resizableImage.width;
+  const newHeight = resizableImage.height;
+  resizableImage.style.width = newWidth + "px";
+  resizableImage.style.height = newHeight + "px";
 });
